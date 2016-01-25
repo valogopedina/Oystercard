ae936dc910830cf62f6ed7afa5028d69d41de6af
Reiss Johnson <reissjohnson@me.com>
2016-01-25 17:39:29 +0000

    challenge #5 done

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -6,5 +6,9 @@ class Oystercard
 		@balance = 0
 	end
 
+  def top_up num
+    @balance += num
+  end
+
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,19 +5,29 @@ RSpec.describe Oystercard do
 
 	subject(:oystercard) {described_class.new}
 
-	context 'oystercard methods' do
-
 		it 'creates an oystercard' do
-		expect(oystercard).to be_a (Oystercard)
+    expect(subject).to be_a (Oystercard)
 		end
-		
-		it 'has a balance 0' do
-		expect(oystercard.balance).to eq(0)
+
+   context 'oystercard has a balance' do
+   it { is_expected.to respond_to :balance }
+
+    it 'has a balance 0' do
+		expect(subject.balance).to eq(0)
 		end
-	end
+   end
+
+   context 'oystercard can be topped up ' do
+   it { is_expected.to respond_to(:top_up).with(1).argument }
+
+   it 'can be topped up with an amount' do
+   	expect{subject.top_up(10)}.to change{ subject.balance }.by 10
+   end
+ end
 
 
 
 
-	
+
+
 end
