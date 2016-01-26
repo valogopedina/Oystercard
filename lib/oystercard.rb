05440e84413ed7ac8dcbb144df14a6c6d30a70dd
valogopedina <valentina.monetta@tiscali.it>
2016-01-26 21:48:46 +0000

    Pushing latest changes, will delete Oystercard.rb later

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -6,16 +6,29 @@ class Oystercard
 
 	def initialize
 		@balance = 0
+		@in_journey = false
 	end
 
-  	def top_up num
-  		maximum_balance = MAXIMUM_BALANCE
-  		raise 'Maximum balance of #{maximum_balance} is exceeded' if (@balance + num) > MAXIMUM_BALANCE
-  			@balance += num
-  	end
+	def top_up num
+		maximum_balance = MAXIMUM_BALANCE
+		raise 'Maximum balance of #{maximum_balance} is exceeded' if (@balance + num) > MAXIMUM_BALANCE
+		@balance += num
+	end
 
-  	def deduct num
-  		@balance -= num
-  	end
+	def deduct num
+		@balance -= num
+	end
+
+	def in_journey?
+		@in_journey
+	end
+
+	def touch_in
+		@in_journey = true
+	end
+
+	def touch_out
+		@in_journey = false
+	end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,50 +1,65 @@
-
-require 'oystercard'
+equire 'oystercard'
 
 RSpec.describe Oystercard do
 
-	subject(:oystercard) {described_class.new}
+  subject(:oystercard) {described_class.new}
 
-		it 'creates an oystercard' do
+  it 'creates an oystercard' do
     expect(subject).to be_a (Oystercard)
-		end
+  end
 
 
-   context 'oystercard has a balance' do
+  context 'oystercard has a balance' do
+
    it { is_expected.to respond_to :balance }
 
-    it 'has a balance 0' do
-		expect(subject.balance).to eq(0)
-		end
+   it 'has a balance 0' do
+     expect(subject.balance).to eq(0)
    end
+ end
 
 
-   context 'oystercard can be topped up ' do
+ context 'oystercard can be topped up ' do
 
    it { is_expected.to respond_to(:top_up).with(1).argument }
 
    it 'can be topped up with an amount' do
-   	expect{subject.top_up(10)}.to change{ subject.balance }.by 10
-   end
+    expect{subject.top_up(10)}.to change{ subject.balance }.by 10
+  end
 
-   it 'raises an error if the maximum balance is exceeded' do
+  it 'raises an error if the maximum balance is exceeded' do
     maximum_balance = Oystercard::MAXIMUM_BALANCE
     oystercard.top_up(maximum_balance)
     expect{oystercard.top_up(5)}.to raise_error 'Maximum balance of #{maximum_balance} is exceeded'
-   end
+  end
+end
 
-   context 'fare can be deducted' do
+context 'fare can be deducted' do
 
-   it { is_expected.to respond_to(:deduct).with(1).argument }
+  it { is_expected.to respond_to(:deduct).with(1).argument }
 
-   it 'subtracts the value of the argument from the existing balance' do
+  it 'subtracts the value of the argument from the existing balance' do
     expect{oystercard.deduct(10)}.to change{oystercard.balance}.by -10
-   end
+  end
+end
 
-   end
+context 'can give the status' do
+ 
+  it 'is initially not in a journey' do
+    expect(oystercard).not_to be_in_journey
+  end
 
- end
+  it 'can touch in' do
+    oystercard.touch_in
+    expect(oystercard).to be_in_journey
+  end
 
+  it 'can touch out' do
+    oystercard.touch_in
+    oystercard.touch_out
+    expect(oystercard).not_to be_in_journey
+  end
 
+end
 
 end
