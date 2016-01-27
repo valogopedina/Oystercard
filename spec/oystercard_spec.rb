e7ba93f8ead1d47dd25ead95994f4f93608ba20e
valogopedina <valentina.monetta@tiscali.it>
2016-01-27 09:26:35 +0000

    refactoring subject in spec file

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,11 +1,11 @@
-equire 'oystercard'
+require 'oystercard'
 
 RSpec.describe Oystercard do
 
   subject(:oystercard) {described_class.new}
 
   it 'creates an oystercard' do
-    expect(subject).to be_a (Oystercard)
+    expect(oystercard).to be_a (Oystercard)
   end
 
 
@@ -14,7 +14,7 @@ RSpec.describe Oystercard do
    it { is_expected.to respond_to :balance }
 
    it 'has a balance 0' do
-     expect(subject.balance).to eq(0)
+     expect(oystercard.balance).to eq(0)
    end
  end
 
@@ -24,7 +24,7 @@ RSpec.describe Oystercard do
    it { is_expected.to respond_to(:top_up).with(1).argument }
 
    it 'can be topped up with an amount' do
-    expect{subject.top_up(10)}.to change{ subject.balance }.by 10
+    expect{oystercard.top_up(10)}.to change{ subject.balance }.by 10
   end
 
   it 'raises an error if the maximum balance is exceeded' do
