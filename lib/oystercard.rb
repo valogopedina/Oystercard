f7aed36ef0d3b60af172f4aea096652d1254923d
valogopedina <valentina.monetta@tiscali.it>
2016-01-28 21:39:42 +0000

    formatting

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,16 +1,16 @@
 
 class Oystercard
 
-	attr_reader :balance, :entry_station, :exit_station, :history, :journey
+  attr_reader :balance, :entry_station, :exit_station, :history, :journey
 
-	MAXIMUM_BALANCE = 90
-	MINIMUM_BALANCE = 1
+  MAXIMUM_BALANCE = 90
+  MINIMUM_BALANCE = 1
   MINIMUM_FARE = 3
 
   def initialize
     @balance = 0
     @history = []
-		journey = Journey.new
+    journey = Journey.new
   end
 
   def top_up num
@@ -19,21 +19,21 @@ class Oystercard
   end
 
   def in_journey?
-		entry_station != nil ? true : false # Ternary Operator
+    entry_station != nil ? true : false # Ternary Operator
 	end
 
 	def touch_in(entry_station)
-		fail 'Your balance is not enough' if balance < MINIMUM_BALANCE
-		@entry_station = entry_station
+    fail 'Your balance is not enough' if balance < MINIMUM_BALANCE
+    @entry_station = entry_station
     #start_journey
-		journey.start_journey(entry_station)
+    journey.start_journey(entry_station)
   end
 
   def touch_out(exit_station)
     deduct(MINIMUM_FARE)
     # @exit_station = exit_station
     # end_journey
-		journey.end_journey(exit_station)
+    journey.end_journey(exit_station)
     @entry_station = nil
   end
 
